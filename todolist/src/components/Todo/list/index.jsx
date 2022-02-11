98497b0a94ebd71e512b57bddb93f7bc47c0a247
Onur Koç <onurkottz@gmail.com>
2022-02-11 18:09:25 +0300

    Written a jQuery function

--- todolist/src/components/Todo/list/index.jsx
+++ todolist/src/components/Todo/list/index.jsx
@@ -1,40 +1,26 @@
-import { useState } from "react";
+import { createElement, useState } from "react";
 import "./style.css";
 
-/*function tick() {
-  
-   console.log("tuş çalışıyor")
- }
 
- onClick={tick} 
-*/
 
 function List ({listin}) {
-  
-  
-  
-  function check(){
-    const list = document.querySelector('.list');
-    list.classList.toggle("checked")
-    console.log("improve this!")
-     
-  }
-    
   
 
   return (
     <div className="row">
       <div className="first">
       <h4>MY AGENDA</h4>
-        <ul>
+        <ul id="unor">
+
         {listin.map( (listin, i) => (
-                <li key={i}>
-                  <span className="list" onClick={check} >
-                    {listin.written}
-                  </span>
-                  </li>
-                
-            ))} 
+        <li key={i}>
+        <span className="list" >
+        {listin.written}
+        </span>
+    </li>
+  
+))}
+        
         </ul>
       </div>
     </div>
